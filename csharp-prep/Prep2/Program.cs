0bc496a421edc8ebd1d41b1f4a7ca1b83afabf6b
Everest wilkey <themount707@gmail.com>
2023-04-22 22:36:23 -0600

    we did it

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your percentage on your grade? ");
+        string score = Console.ReadLine();
+
+        int ScoreNumber = int.Parse(score);
+
+        if (ScoreNumber >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (ScoreNumber >= 80)
+        {
+            Console.WriteLine("You have an B");
+        }
+        else if (ScoreNumber >= 70)
+        {
+            Console.WriteLine("You have an C");
+        }
+        else if (ScoreNumber >= 60)
+        {
+            Console.WriteLine("You have an D");
+        }
+        else if (ScoreNumber < 60)
+        {
+            Console.WriteLine("You have an F");
+        }
+        if (ScoreNumber >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("Ummm.. you didn't make it....");
+        }
     }
 }
