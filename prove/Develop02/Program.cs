66730b24d4f74924372053c573e59f1ad4485f36
everestwilkey <themount707@gmail.com>
2023-05-08 10:24:01 -0600

    Add the entry jurnal class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+namespace Develop02
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Entry myentry = new Entry();
+            myentry.Hold("What is your name?", "mattew Manly", "8 May 2023");
+
+            Journal journal = new Journal();
+            journal.AddEntry(myentry);
+
+            List<Entry> entries = journal.GetAllEntries();
+            foreach (Entry entry in entries)
+            {
+                string messages = entry.ConvertToString();
+                Console.WriteLine(messages);
+            }
+
+
+
+        }
     }
 }
