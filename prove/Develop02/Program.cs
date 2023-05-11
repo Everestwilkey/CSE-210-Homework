3167aea75025aca92b67341e56aefbc1e2b007ba
everestwilkey <themount707@gmail.com>
2023-05-10 18:19:38 -0600

    this is addeding save functionality

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,18 +6,35 @@ namespace Develop02
     {
         static void Main(string[] args)
         {
-            Entry myentry = new Entry();
-            myentry.Hold("What is your name?", "mattew Manly", "8 May 2023");
-
-            Journal journal = new Journal();
-            journal.AddEntry(myentry);
-
-            List<Entry> entries = journal.GetAllEntries();
-            foreach (Entry entry in entries)
+            
+            List<string> choices = new List<string> {"1. Write", "2. Display", "3. Load", "4. Save", "5. Quit"};
+            foreach(string items in choices)
             {
-                string messages = entry.ConvertToString();
-                Console.WriteLine(messages);
+                Console.WriteLine(items);
             }
+            Console.Write("Choose what you would like to do");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Entry myentry = new Entry();
+                    Console.WriteLine("What is todays date?"); 
+                    myentry.date = Console.ReadLine();
+                    myentry.prompt = "What did you have for dinner?"; 
+                    Console.WriteLine(myentry.prompt);
+                    myentry.response = Console.ReadLine();
+                    Journal journal = new Journal();
+                    journal.AddEntry(myentry);
+                    break;
+            }
+
+
+            //List<Entry> entries = journal.GetAllEntries();
+            //foreach (Entry entry in entries)
+                //{
+                //    string messages = entry.ConvertToString();
+                //    Console.WriteLine(messages);
+                //}
 
 
 
