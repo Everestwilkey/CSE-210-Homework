d6b643ece3259adcccc0f633ffbffdd3c48b508d
Everest wilkey <themount707@gmail.com>
2023-06-13 22:55:51 -0600

    this is your stander end of day commit

--- prove/Develop04/Menu.cs
+++ prove/Develop04/Menu.cs
@@ -9,52 +9,67 @@ namespace Develop04
     {
         public void run()
         {
-            BodyOfWater bodyofwater = new BodyOfWater();
-            Console.Write("Lake[2] or River[1]: ");
-            string choice = Console.ReadLine();
-            if (choice == "2")
+            bool end = false;
+            while (end != true)
             {
-                Console.Write("Enter the name of the Lake: ");
-                string lakechoice = Console.ReadLine();
-
-                switch (lakechoice.ToLower())
+                BodyOfWater bodyofwater = new BodyOfWater();
+                Console.Write("Lake[2] or River[1]: ");
+                string choice = Console.ReadLine();
+                if (choice == "2")
                 {
-                    case "henrys":
-                        HenrysLake henrysLake = new HenrysLake();
-                        string flies = henrysLake.GetLakeInfo();
-                        Console.WriteLine(flies);
-                        bodyofwater.canWalk(henrysLake.returnwalkinfo());
-                        bodyofwater.canfloat(henrysLake.returnfloatsinfo());
+                    Console.Write("Enter the name of the Lake: ");
+                    string lakechoice = Console.ReadLine();
 
-                        break;
-                    case "Rirey":
-                        break;
+                    switch (lakechoice.ToLower())
+                    {
+                        case "henrys":
+                            HenrysLake henrysLake = new HenrysLake();
+                            string flies = henrysLake.GetLakeInfo();
+                            Console.WriteLine(flies);
+                            bodyofwater.canWalk(henrysLake.returnwalkinfo());
+                            bodyofwater.canfloat(henrysLake.returnfloatsinfo());
+
+
+                            break;
+                        case "Rirey":
+                            break;
+                    }
+
                 }
-
-            }
-            else if (choice == "1")
-            {
-                Console.Write("Enter the name of the River: ");
-                string riverchoice = Console.ReadLine();
-
-                switch (riverchoice.ToLower())
+                else if (choice == "1")
                 {
-                    case "snake":
-                        Snake snakeriver = new Snake();
-                        string flies = snakeriver.GetRiverInfo();
-                        Console.WriteLine(flies);
-                        break;
-                    case "warm":
-                        break;
+                    Console.Write("Enter the name of the River: ");
+                    string riverchoice = Console.ReadLine();
+
+                    switch (riverchoice.ToLower())
+                    {
+                        case "snake":
+                            Snake snakeriver = new Snake();
+                            string flies = snakeriver.GetRiverInfo();
+                            Console.WriteLine(flies);
+                            break;
+                        case "warm":
+                            break;
+                    }
+
                 }
+                else
+                {
 
+                }
             }
-            else
+
+
+        }
+        public void Stopping()
+        {
+            Console.WriteLine("Would you like to end? [1]Yes [2]No ");
+            string _end = Console.Read();
+            if (_end == )
             {
-
+                end == true;
+                return end;
             }
-
-
         }
     }
 }
